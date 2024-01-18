1d40833e0d5c839e46b96e75ccd1bb8d9fb540b3
s1270146 <s1270146@gmail.com>
2024-01-19 03:31:35 +0900

    フォント追加

---
+++ utils/font.ts
@@ -0,0 +1,13 @@
+import {Jacques_Francois, Jacques_Francois_Shadow} from 'next/font/google'
+
+export const jacquesFrancois = Jacques_Francois({
+    weight: "400",
+    subsets: ["latin"],
+    display: "swap"
+});
+
+export const jacquesFrancoisShadow = Jacques_Francois_Shadow({
+    weight: "400",
+    subsets: ["latin"],
+    display: "swap"
+});
