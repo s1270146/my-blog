6da8cb548ab5a02b03ee19228ea2d6677b5479be
s1270146 <s1270146@gmail.com>
2024-01-23 04:03:12 +0900

    create footer

--- components/common/Footer.tsx
+++ components/common/Footer.tsx
@@ -2,7 +2,7 @@ import { jacquesFrancois } from "@/utils/font";
 
 const Footer = () => {
     return (
-        <footer className={jacquesFrancois.className + " flex justify-center w-full absolute bottom-0"}>
+        <footer className={jacquesFrancois.className + " flex justify-center w-full fixed bottom-0 bg-white"}>
             <p>&copy; 2024 Koyo Mori</p>
         </footer>
     );
