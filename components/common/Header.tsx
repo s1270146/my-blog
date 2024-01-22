d1ce595f0397a72d20707f7022f529d1003405a6
s1270146 <s1270146@gmail.com>
2024-01-23 04:03:23 +0900

    create header

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const Header = () => {
     return (
-        <header className="pt-12 flex justify-center bg-light-grey top-0">
+        <header className="pt-12 flex justify-center bg-light-gray top-0">
             <Link href="/">
                 <p className={`${jacquesFrancoisShadow.className} inline-block  text-5xl`}>MKは勉強したい</p>
             </Link>
--- components/common/SubHeader.tsx
+++ components/common/SubHeader.tsx
@@ -1,14 +1,17 @@
 import { jacquesFrancois } from "@/utils/font";
+import Link from "next/link";
 
 const SubHeader = () => {
     const textButtonClassName: string = jacquesFrancois.className + " text-white text-3xl";
     return (
-        <div className="w-full flex justify-center items-center gap-4 bg-mid-grey py-1">
+        <div className="w-full flex justify-center items-center gap-4 bg-mid-gray py-1 sticky top-0">
             <div className={textButtonClassName}>Article</div>
             <div className={textButtonClassName}>Work</div>
             <div className={textButtonClassName}>Daily</div>
             <div className={textButtonClassName}>Research</div>
-            <div className={textButtonClassName}>Profile</div>
+            <Link href="./profile">
+                <div className={textButtonClassName}>Profile</div>
+            </Link>
         </div>
     );
 }
