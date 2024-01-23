a473df58b35bb8eddc358a16d9393c93024586f0
s1270146 <s1270146@gmail.com>
2024-01-24 02:56:00 +0900

    Notionデータベースから最新の3つのデータを取得する関数

---
+++ utils/get-recent-article.ts
@@ -0,0 +1,44 @@
+import notion from "@/utils/notion";
+import { QueryDatabaseResponse } from "@notionhq/client/build/src/api-endpoints";
+
+type getRecentArticleProps = {
+    category?: string
+}
+
+export const getRecentArticle = (props: getRecentArticleProps):Promise<QueryDatabaseResponse> => {
+    const isDefault = typeof props.category != "string"
+    const defaultFilter = {
+        property: "IS_PUBLISHED",
+        checkbox: {
+            equals: true,
+        }
+    }
+
+    const filter = isDefault
+        ? defaultFilter
+        : {
+            and: [
+                defaultFilter,
+                {
+                    property: "CATEGORY",
+                    select: {
+                        equals: props.category!
+                    }
+                }
+            ],
+        }
+
+    const dbId = process.env.NOTION_DB_ID as string
+    const data = notion.databases.query({
+        database_id: dbId,
+        page_size: 3,
+        filter: filter,
+        sorts: [
+            {
+                property: "CREATED_AT",
+                direction: "descending"
+            }
+        ],
+    })
+    return data;
+}
