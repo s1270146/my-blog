e3bc0a66559f44cffa8c8d6f1354c881f44fb939
s1270146 <s1270146@gmail.com>
2024-01-24 02:55:12 +0900

    高さ調整

--- components/common/SubHeader.tsx
+++ components/common/SubHeader.tsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 const SubHeader = () => {
     const textButtonClassName: string = jacquesFrancois.className + " text-white text-3xl";
     return (
-        <div className="w-full flex justify-center items-center gap-4 bg-mid-gray py-1 sticky top-0">
-            <div className={textButtonClassName}>Article</div>
+        <div className="w-full flex justify-center items-center gap-4 bg-mid-gray py-1 sticky top-0 fixed z-50">
+            <div className={textButtonClassName}>Blog</div>
             <div className={textButtonClassName}>Work</div>
             <div className={textButtonClassName}>Daily</div>
             <div className={textButtonClassName}>Research</div>
