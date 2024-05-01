1df2e1d879a87a9d433f17d88953813e17bb55a3
s1270146 <s1270146@gmail.com>
2024-05-01 19:56:31 +0900

    デフォルトの画像を設定

--- components/article/ArticleCard.tsx
+++ components/article/ArticleCard.tsx
@@ -1,5 +1,7 @@
 import { Article } from "@/models/article";
+import Image from "next/image";
 import Link from "next/link";
+import defaultPhoto from '@/public/default.png';
 
 type ArticleCardProps = {
     article: Article
@@ -8,27 +10,22 @@ type ArticleCardProps = {
 const ArticleCard = (props: ArticleCardProps) => {
     return (
         <div className="h-60 md:h-80 bg-light-gray relative md:mb-0 mb-4 border">
-            <div className="w-full h-[7.5rem] md:h-40 bg-mid-gray">
-                {
-                    typeof props.article.imgUrl !== "undefined"
-                    ? <img className="w-full h-[7.5rem] md:h-40 object-cover" src={props.article.imgUrl} alt=''></img>
-                    : <div className=""><p className="text-center text-3xl">No Image</p></div>
-                }
-            </div>
-            <div>
-                <div className="flex justify-between mx-1">
-                    <div className="text-mid-gray">{props.article.createdAt}</div>
-                    <div>{props.article.category}</div>
+            <Link href={"/article/"+props.article.id}>
+                <div className="w-full h-[7.5rem] md:h-40 bg-mid-gray">
+                    {
+                        typeof props.article.imgUrl !== "undefined"
+                        ? <img className="w-full h-[7.5rem] md:h-40 object-cover" src={props.article.imgUrl} alt='catch image'></img>
+                        : <Image src={defaultPhoto} alt="default photo" className="w-full h-[7.5rem] md:h-40 object-cover" />
+                    }
                 </div>
-                <p className="text-ellipsis overflow-hidden w-full h-14 lg:h-20 text-center lg:text-2xl text-xl">{props.article.title}</p>
-                <div className="absolute bottom-0 w-full">
-                        <div className="w-28 text-center mx-auto mb-2 h-6 md:h-10 bg-white md:leading-10 border border-black">
-                            <Link href={"/article/"+props.article.id}>
-                                READ MORE
-                            </Link>
-                        </div>
+                <div>
+                    <div className="flex justify-between mx-1">
+                        <p className="text-mid-gray">{props.article.createdAt}</p>
+                        <p className="text-mid-gray">{props.article.category.toUpperCase()}</p>
+                    </div>
+                    <p className="text-ellipsis overflow-hidden w-full h-24 md:h-32 text-center lg:text-2xl text-xl">{props.article.title}</p>
                 </div>
-            </div>
+            </Link>
         </div>
     );
 }
